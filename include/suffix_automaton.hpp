3a0aa6e04160464d4664b99ac65d005c49f89a86
Nanako7-ix <nanako7.ix@gmail.com>
2026-07-05 07:27:08 +0800

    fixed(suffix_automaton): 调整了函数签名

--- include/suffix_automaton.hpp
+++ include/suffix_automaton.hpp
@@ -8,33 +8,33 @@ namespace nnk {
 template <std::size_t N>
 class suffix_automaton {
 public:
-  suffix_automaton() : cnt(0), sam(2) {}
+  constexpr suffix_automaton() : cnt(0), sam(2) {}
 
-  void reserve(std::size_t n) {
+  constexpr void reserve(std::size_t n) {
     sam.reserve(n);
   }
 
-  int extend(int p, int ch) {
+  constexpr int extend(int p, int ch) {
     int u = new_node();
     sam[u].len = sam[p].len + 1;
 
     for (; p && next(p, ch) == 0; p = link(p)) {
-      sam[p].next(ch) = u;
+      sam[p].next[ch] = u;
     }
 
     if (p == 0) {
-      sam.link[u] = 1;
+      sam[u].link = 1;
     } else {
       int q = next(p, ch);
-      if (sam.len[p] + 1 == len(q)) {
-        sam.link[u] = q;
+      if (sam[p].len + 1 == len(q)) {
+        sam[u].link = q;
       } else {
         int r = new_node();
 
-        sam[r].len     = len(p) + 1;
-        sam[r].link    = link(q);
-        sam[r].next    = next(q);
-        sam[q].link[q] = sam[u].link = r;
+        sam[r].len = len(p) + 1;
+        sam[r].link = link(q);
+        sam[r].next = next(q);
+        sam[q].link = sam[u].link = r;
         for (; p && next(p, ch) == q; p = link(p)) {
           sam[p].next[ch] = r;
         }
@@ -44,32 +44,32 @@ public:
     return u;
   }
 
-  constexpr static std::size_t sigma() noexcept {
+  static constexpr std::size_t sigma() noexcept {
     return N;
   }
 
-  std::size_t size() const noexcept {
+  constexpr std::size_t size() const noexcept {
     return sam.size() - 1;
   }
 
-  std::size_t count() const noexcept {
+  constexpr std::size_t count() const noexcept {
     return cnt;
   }
 
-  int len(int u) const {
+  constexpr int len(int u) const noexcept {
     return sam[u].len;
   }
-  int link(int u) const {
+  constexpr int link(int u) const noexcept {
     return sam[u].link;
   }
-  int next(int u, int ch) const {
+  constexpr int next(int u, int ch) const noexcept {
     return sam[u].next[ch];
   }
-  std::array<int, N> next(int u) const {
+  constexpr std::array<int, N> next(int u) const noexcept {
     return sam[u].next;
   }
 
-  auto get_tree() const -> std::vector<std::vector<int>> {
+  constexpr auto get_tree() const -> std::vector<std::vector<int>> {
     std::size_t sz = size();
 
     std::vector<std::vector<int>> adj(sz + 1);
@@ -88,7 +88,7 @@ private:
   std::size_t cnt = 0;
   std::vector<node> sam;
 
-  int new_node() {
+  constexpr int new_node() {
     sam.emplace_back();
     return sam.size() - 1;
   }
